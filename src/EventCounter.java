88938756dbfa7437a5ddbb639a07e2d3629aff2d
Utkarsh Raj <utkarsh436@gmail.com>
2020-05-12 02:07:08 -0700

    third

--- src/EventCounter.java
+++ src/EventCounter.java
@@ -1,16 +1,11 @@
-import java.sql.Array;
-import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.*;
 
 public class EventCounter {
     private int counter = 0;
-//    private Map<String, Integer> map = new HashMap<>();
-//    private Map<String, Integer> map = new LinkedHashMap<>();
-
-
     private List<Timestamp> timestamp = new ArrayList<>();
     private List<Integer> count = new ArrayList<>();
+
     public EventCounter(){
     }
 
@@ -19,36 +14,55 @@ public class EventCounter {
     }
 
     public void incrementCounter(Timestamp t) {
-        counter += 1;
-        int left = 0;
-        int right = timestamp.size();
+        if(timestamp.size() == 0 || timestamp.size() == 1){
+            timestamp.add(t);
+            count.add(1);
+            return;
+        }
 
+        counter += 1;
+
+        int idx = binarySearch(t);
+
+        if(idx == timestamp.size()-1){
+            timestamp.add(t);
+            count.add(1);
+        }
+        else{
+            timestamp.add(idx, t);
+            count.add(idx, 1);
+        }
+    }
+
+    public int returnCountOverTime(String start, String end){
+
+        Timestamp startTime = Timestamp.valueOf(start);
+        Timestamp endTime = Timestamp.valueOf(end);
+
+        int startIdx = binarySearch(startTime);
+        System.out.print(startIdx);
+
+        int endIdx = binarySearch(endTime);
+
+        int sum = 0;
+        for(int i = startIdx; i <=endIdx; i++){
+            sum += count.get(i);
+        }
+        return sum;
+    }
+
+    private int binarySearch(Timestamp target){
+        int left = 0;
+        int right = timestamp.size()-1;
         while(left < right){
             int mid = left + (right - left) /2 ;
-            int val = timestamp.get(mid).compareTo(t);
+            int val = timestamp.get(mid).compareTo(target);
             if(val < 0){
                 left = mid + 1;
             }
             else right = mid;
         }
-        timestamp.add(left, t);
-        count.add(left, 1);
-
-
-    }
-
-    public int returnCountOverTime(String start, String end){
-        int count = 0;
-//        int startIdx = map.get(start);
-//        int endIdx = map.get(end);
-//        for(int i = startIdx; i <= endIdx; i++){
-//            count += list.get(i);
-//        }
-//        return count;
-
-        // find the first key is larger than the start
-
-        return 0;
+        return left;
     }
 
 }
--- src/main.java
+++ src/main.java
@@ -6,13 +6,28 @@ public class main {
 
     public static void main(String[] args) throws InterruptedException {
         EventCounter ec = new EventCounter();
-        ec.incrementCounter(new Timestamp(System.currentTimeMillis()));
-        sleep(100);
-        ec.incrementCounter(new Timestamp(System.currentTimeMillis()));
-        sleep(100);
+
+        Timestamp first = new Timestamp(System.currentTimeMillis());
+        sleep(1000);
+        Timestamp second = new Timestamp(System.currentTimeMillis());
+        sleep(1000);
+        Timestamp third = new Timestamp(System.currentTimeMillis());
+        sleep(1000);
+        Timestamp fourth = new Timestamp(System.currentTimeMillis());
+        sleep(1000);
+        Timestamp fifth = new Timestamp(System.currentTimeMillis());
+
+
+
+        ec.incrementCounter(first);
+        ec.incrementCounter(second);
+        ec.incrementCounter(third);
+        ec.incrementCounter(fourth);
+        ec.incrementCounter(fifth);
+
         System.out.println(ec.getCounter());
 
-        System.out.println(ec.returnCountOverTime("", ""));
+        System.out.println(ec.returnCountOverTime(second.toString(), fourth.toString()));
 
 
     }
